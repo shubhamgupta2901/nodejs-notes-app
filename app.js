70e0c9b4bb5947ad95225ca4bfa94b915e3ad25b
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-13 21:57:50 +0530

    Adding essential yarg commands

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 console.log(chalk.blue.inverse('Success!'));
 
-const command = process.argv[2];
+//customize yargs
+yargs.version('1.0.1');
 
-switch(command){
-    case 'add': 
-        console.log('Add note');
-        break;
-    case 'remove':
-        console.log('Remove note');
-        break;
-    default:
-        console.log(chalk.red('Unknown command!'));
-        break;
+const addNewNote = () => {
+    console.log('Adding a new note');
 }
+
+const removeNote = () => {
+    console.log('Remove a note.')
+}
+
+const listNotes = () => {
+    console.log('List notes');
+}
+
+const readNote = () => {
+    console.log('Read a note');
+}
+
+//Create add command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: addNewNote,
+});
+
+// Create Remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove an existing note',
+    handler: removeNote,
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all the notes',
+    handler: listNotes,
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: readNote,
+})
+
+console.log(yargs.argv);
+
+
+
