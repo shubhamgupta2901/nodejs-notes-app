e905c67f279115e3dd05a70e214304ee03b94749
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-13 22:36:27 +0530

    Adding properties to commands via yargs

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ console.log(chalk.blue.inverse('Success!'));
 //customize yargs
 yargs.version('1.0.1');
 
-const addNewNote = () => {
-    console.log('Adding a new note');
+const addNewNote = (argv) => {
+   console.log(`Adding a new note with title: ${chalk.cyan.bold.underline(argv.title)} and body: ${chalk.cyan.underline.bold(argv.body)}`);
 }
 
 const removeNote = () => {
@@ -25,7 +25,20 @@ const readNote = () => {
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: addNewNote,
+    builder: {
+        //creating new properties for the command
+        title: {
+            describe: 'Title of the note',
+            demandOption: true, //compulsory 
+            type: 'string', //of type string
+        }, 
+        body: {
+            describe: 'Body of your note',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: (argv)=>addNewNote(argv),
 });
 
 // Create Remove command 
@@ -47,7 +60,8 @@ yargs.command({
     handler: readNote,
 })
 
-console.log(yargs.argv);
+//its important to first parse the arguments with yargs, otherwise yargs does not parse them implicitly.
+yargs.parse()
 
 
 
