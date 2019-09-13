f51e9895ca656fe748c8677197f1904dd16eac10
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-13 21:29:13 +0530

    Getting inputs from command line

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const chalk = require('chalk');
 console.log(chalk.blue.inverse('Success!'));
+
+const command = process.argv[2];
+
+switch(command){
+    case 'add': 
+        console.log('Add note');
+        break;
+    case 'remove':
+        console.log('Remove note');
+        break;
+    default:
+        console.log(chalk.red('Unknown command!'));
+        break;
+}
